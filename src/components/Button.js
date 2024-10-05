ce6ee0323553995c66f2f2ef7545b256f5c14cda
haristeelers <haristeelers@gmail.com>
2024-10-04 20:52:47 -0400

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -21,7 +21,7 @@ export const Button = ({
         ? buttonSize
         : SIZES[0];
     return(
-        <Link to='blog' className='btn-mobile'>
+        <Link to='/sign-up' className='btn-mobile'>
             <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                 onClick={onClick}
