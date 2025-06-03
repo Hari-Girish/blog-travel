eef9ff518a27cd01f5b79849729a3fa56db712e5
Hari Girish <haristeelers@gmail.com>
2025-06-03 16:56:09 -0400

    Update SignCards.js

--- src/components/SignCards.js
+++ src/components/SignCards.js
@@ -12,13 +12,13 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/newport.jpg'
+              src='images/newport.jpeg'
               text='Newport Aquarium was such an amazing place to discover the world under the sea. Definetly a one of the memorable experience from when I was young.'
               label='Newport Aquarium'
               path='https://www.newportaquarium.com/?utm_source=google_ads&utm_medium=PMax-naq&gad_source=1'
             />
             <CardItem
-              src='images/zoo.jpg'
+              src='images/zoo.jpeg'
               text='The Cincinnati Zoo definetly fascinated me with huge collection of animals they had to offer. All the animals seemed to be well treated and happy to see us visit them. Definetly lives up to the Inernationally Acreddited Title.'
               label='Cincinnati Zoo'
               path='https://cincinnatizoo.org/?gad_source=1'
@@ -31,14 +31,14 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
           <CardItem
-              src='images/chicago.jpg'
+              src='images/chicago.jpeg'
               text='Exploring Chicago was such a fun experience because it is such a big city. My highlights were Willis Tower which was one of the largest buildings in the world. 
               The pier had many cool attractions like the ferris wheel from where you can get an amazing view of the city. The Deep Dish Pizza is not for everyone and that is okay!'
               label='Chicago'
               path='https://www.choosechicago.com/'
             />
             <CardItem
-              src='images/Pitt.jpg'
+              src='images/Pitt.jpeg'
               text='My first time here left me speechless. So much that I came back to this amazing city many times later. The Carnegie Mellon Science Center and the Duquesne Incline
                that takes you to the top of the city left me coming back for more. Just walking around the city and exploring the surplus of local parks and bridges is an attraction of in itself. 
                Highly recommend taking a visit to this beautiful city.'
@@ -46,7 +46,7 @@ function SignCards() {
               path='https://www.visitpittsburgh.com/'
             />
             <CardItem
-              src='images/Hocking.jpg'
+              src='images/Hocking.jpeg'
               text='Rented one of the cabins here and it is the best way to experience the Hocking Hills National Park. Ziplining and taking a scenic walk through 
               one of the many walking trails that this park has to offer will leave you wanting to extend your stay here.'
               label='Hocking Hills'
@@ -67,7 +67,7 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/Dayton.jpg'
+              src='images/Dayton.jpeg'
               text='My intial aspiration of becoming a commercial airline pilot was born in this museum. This place has every airplane you can possibly imagine. All your airplane curiosities will be answered here.'
               label='Dayton Airforce Museum'
               path='https://www.nationalmuseum.af.mil/'
@@ -87,7 +87,7 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/saguatuck.jpg'
+              src='images/saguatuck.jpeg'
               text='My dad spontaneously decided that we were going to this beach for a day trip. That was one of the best decisions 
               he had ever made when it came to vacations because Saguatuck was one of the best beaches I had ever been to. The white sand was so soft, the water was 
               the perfect temperature, and it was a very clean beach. It also had quite a bit of waves for a lake.'
@@ -109,7 +109,7 @@ function SignCards() {
               path='https://www.niagarafallsstatepark.com/'
             />
             <CardItem
-              src='images/diego.jpg'
+              src='images/diego.jpeg'
               text='Absolutely gorgeous city. The San-Diego Zoo was so massive that people were taking buses just to get around. The sunset boat cruise along the coast of San Diego was a site to see. '
               label='San Diego'
               path='https://www.niagarafallsstatepark.com/'
@@ -122,7 +122,7 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/munnar.jpg'
+              src='images/munnar.jpeg'
               text='I could not be more serious when I say this place looks exactly like the pictures. Even just the road trip to this mountaneous tea garden preserve left me speechless.
               When we were not drooling over the views we took a tour of the tea factory and jeep tours through this beautiful landscape. If you want to take a hot air baloon trip I would book a couple days in advance.
               Also we had hired a driver to take us around because the roads are really narrow and our local driver took us to the best food and tourist spots. '
@@ -137,7 +137,7 @@ function SignCards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/myrtle.jpg'
+              src='images/myrtle.jpeg'
               text='I went here during late March and the water was still way too cold to be swimming in it. So we explored the local cuisine and mini golf courses. 
               There was suprisingly a lot of mini-golf courses around here. We also went to this burger restaurant call River City Cafe which was fantastic. 
               Definetly recommend here in the summer when it is warmer and everything is open.'
